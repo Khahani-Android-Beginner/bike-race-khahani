d34ebdf374489a9c2cd046bd26d38364061fc719
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-06-19 00:42:25 -0700

    تکمیل کلاس دنده

--- src/model/Gear.java
+++ src/model/Gear.java
@@ -5,4 +5,21 @@ public class Gear {
      * model
      * size (between 0 and 10)
      */
+
+    public String model;
+
+    private int mSize;
+
+    public int getSize(){
+        return mSize;
+    }
+
+    public void setSize(int size){
+        if (size < 0 && size > 10){
+            System.out.println("size is not valid");
+            return;
+        }
+
+        mSize = size;
+    }
 }
